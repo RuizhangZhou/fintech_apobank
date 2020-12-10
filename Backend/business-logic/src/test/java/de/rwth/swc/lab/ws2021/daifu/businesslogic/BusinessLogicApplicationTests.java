c91fa50b9c3fcd636f659c1c82e4da7aba5404fb
Martin <mn.vichev@gmail.com>
2020-12-10 16:43:13 +0200

    Adding Business Logic Project

---
+++ Backend/business-logic/src/test/java/de/rwth/swc/lab/ws2021/daifu/businesslogic/BusinessLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rwth.swc.lab.ws2021.daifu.businesslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
