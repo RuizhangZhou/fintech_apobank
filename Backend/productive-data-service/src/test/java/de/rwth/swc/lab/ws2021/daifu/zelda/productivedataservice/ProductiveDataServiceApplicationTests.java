018eac1cb203fb83e864c98707ac3438bc840ebe
Robin Jaschke <robin.jaschke@rwth-aachen.de>
2020-12-20 10:03:18 +0100

    initialize Spring project

---
+++ Backend/productive-data-service/src/test/java/de/rwth/swc/lab/ws2021/daifu/zelda/productivedataservice/ProductiveDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rwth.swc.lab.ws2021.daifu.zelda.productivedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductiveDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
