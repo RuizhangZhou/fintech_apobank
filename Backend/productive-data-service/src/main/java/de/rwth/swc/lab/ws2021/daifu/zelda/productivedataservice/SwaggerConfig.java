21438faeb137e758c44decef2a6096c3950d15a0
Robin Jaschke <robin.jaschke@rwth-aachen.de>
2020-12-20 11:35:19 +0100

    configure swagger

---
+++ Backend/productive-data-service/src/main/java/de/rwth/swc/lab/ws2021/daifu/zelda/productivedataservice/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package de.rwth.swc.lab.ws2021.daifu.zelda.productivedataservice;
+
+import com.fasterxml.classmate.TypeResolver;
+import de.rwth.swc.lab.ws2021.daifu.zelda.productivedataservice.models.InputData;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api(TypeResolver typeResolver) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("de.rwth.swc.lab.ws2021.daifu.zelda.productivedataservice"))
+                .paths(PathSelectors.any())
+                .build()
+                .additionalModels(typeResolver.resolve(InputData.class));
+    }
+}
