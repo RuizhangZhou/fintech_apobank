58644d5ee45c018e5491f9a611267a277f2c9069
Robin Jaschke <robin.jaschke@rwth-aachen.de>
2020-12-23 08:01:22 +0100

    initialize batch service structure

---
+++ Backend/batch-process/src/main/java/de/rwth/swc/lab/ws2021/daifu/zelda/batchprocess/BatchProcessApplication.java
@@ -0,0 +1,13 @@
+package de.rwth.swc.lab.ws2021.daifu.zelda.batchprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessApplication.class, args);
+	}
+
+}
---
+++ Backend/batch-process/src/test/java/de/rwth/swc/lab/ws2021/daifu/zelda/batchprocess/BatchProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rwth.swc.lab.ws2021.daifu.zelda.batchprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
