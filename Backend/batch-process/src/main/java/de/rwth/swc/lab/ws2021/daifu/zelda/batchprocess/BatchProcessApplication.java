03acd57166fc1bfa1cced03bd0837ef1fb4166f2
jan_w <jan.christian.wieczorek@rwth-aachen.de>
2021-01-07 09:59:23 +0100

    Add update CustomerInputData and update all InputData

--- Backend/batch-process/src/main/java/de/rwth/swc/lab/ws2021/daifu/zelda/batchprocess/BatchProcessApplication.java
+++ Backend/batch-process/src/main/java/de/rwth/swc/lab/ws2021/daifu/zelda/batchprocess/BatchProcessApplication.java
@@ -1,11 +1,7 @@
 package de.rwth.swc.lab.ws2021.daifu.zelda.batchprocess;
 
-import de.rwth.swc.lab.ws2021.daifu.zelda.batchprocess.models.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import de.rwth.swc.lab.ws2021.daifu.zelda.batchprocess.api.transformInputController;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class BatchProcessApplication {
