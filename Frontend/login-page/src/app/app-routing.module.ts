a8a71e3661a19a007f2bccb2efe41f5b2b330657
Almut Herzog <almut.herzog@rwth-aachen.de>
2021-01-19 16:38:29 +0100

    Fix accounts view

--- Frontend/login-page/src/app/app-routing.module.ts
+++ Frontend/login-page/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from '../app/home/home.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { CreditsComponent } from './credits/credits.component';
 import { InvestmentsComponent } from './investments/investments.component';
