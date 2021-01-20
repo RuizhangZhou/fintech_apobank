1a2e6dfeb97645df543a9ce3b53005af99d4c31c
Almut Herzog <almut.herzog@rwth-aachen.de>
2021-01-20 10:26:42 +0100

    Remove hovering over image in side bar

--- Frontend/login-page/src/app/app-routing.module.ts
+++ Frontend/login-page/src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from '../app/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { CreditsComponent } from './credits/credits.component';
 import { InvestmentsComponent } from './investments/investments.component';
 import { LogoutComponent } from './logout/logout.component';
 import { MyDataComponent } from './my-data/my-data.component';
-
-import{RegComponent} from '../app/reg/reg.component'
-import{LogComponent} from '../app/log/log.component'
+import{RegComponent} from './reg/reg.component'
+import{LogComponent} from './log/log.component'
 
 const routes: Routes = [
-  {path:'',redirectTo:'log',pathMatch:'full'},
-  {path:'reg', component:RegComponent},
-  {path:'log', component:LogComponent},
+  { path: '', redirectTo:'log',pathMatch:'full' },
+  { path: 'reg', component: RegComponent },
+  { path: 'log', component: LogComponent },
   { path: 'home', component: HomeComponent },
   { path: 'accounts', component: AccountsComponent },
   { path: 'credits', component: CreditsComponent },
@@ -25,6 +24,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
