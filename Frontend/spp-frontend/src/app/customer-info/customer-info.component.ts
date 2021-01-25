bd15b5043548d8fac47f7654b3e601ca2bf7be76
Jan <jan.christian.wieczorek@rwth-aachen.de>
2021-01-25 17:30:02 +0100

    Prettify function

--- Frontend/spp-frontend/src/app/customer-info/customer-info.component.ts
+++ Frontend/spp-frontend/src/app/customer-info/customer-info.component.ts
@@ -13,6 +13,8 @@ import {RestService, Customer} from '../rest.service';
 })
 export class CustomerInfoComponent implements OnInit {
   customer: Customer = <Customer> {};
+  pCustomer: Customer = <Customer> {};
+
   //customers = CUSTOMERS;
 
   //selectedCustomer: Customer  = this.customer;
@@ -22,7 +24,7 @@ export class CustomerInfoComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCustomer(this.rest.test_customer_number);
-
+    this.prettifyCustomer()
     /*this.navigation.currentSelected = 'Meine Daten';*/
   }
   onSelect(customer: Customer): void{
@@ -34,5 +36,101 @@ export class CustomerInfoComponent implements OnInit {
       this.customer = resp;
       console.log(this.customer);
     });
+  }
+
+  prettifyCustomer(): void {
+    switch(this.customer.job){
+      case "ADMIN": {
+        this.pCustomer.job = "Administrator";
+        break;
+      }
+      case "UNKNOWN": {
+        this.pCustomer.job = "Unbekannt";
+        break;
+      }
+      case "UNEMPLOYED": {
+        this.pCustomer.job = "Arbeitslos";
+        break;
+      }
+      case "MANAGEMENT": {
+        this.pCustomer.job = "Management";
+        break;
+      }
+      case "HOUSEMAID": {
+        this.pCustomer.job = "Hausmädchen";
+        break;
+      }
+      case "ENTREPRENEUR": {
+        this.pCustomer.job = "Unternehmer";
+        break;
+      }
+      case "STUDENT": {
+        this.pCustomer.job = "Student";
+        break;
+      }
+      case "BLUE_COLLAR": {
+        this.pCustomer.job = "Arbeiter";
+        break;
+      }
+      case "SELF_EMPLOYED": {
+        this.pCustomer.job = "Selbständig";
+        break;
+      }
+      case "RETIRED": {
+        this.pCustomer.job = "Ruhestand";
+        break;
+      }
+      case "TECHNICIAN": {
+        this.pCustomer.job = "Techniker";
+        break;
+      }
+      case "SERVICES": {
+        this.pCustomer.job = "Dienstleistung";
+        break;
+      }
+    }
+    switch(this.customer.relationshipStatus){
+      case "MARRIED": {
+        this.pCustomer.job = "Verheiratet";
+        break;
+      }
+      case "REGISTERED_PARTNERSHIP": {
+        this.pCustomer.job = "Registrierte Partnerschaft";
+        break;
+      }
+      case "DIVORCED": {
+        this.pCustomer.job = "Geschieden";
+        break;
+      }
+      case "SINGLE": {
+        this.pCustomer.job = "Single";
+        break;
+      }
+      case "WIDOWED": {
+        this.pCustomer.job = "Verwitwet";
+        break;
+      }
+    }
+    switch(this.customer.education){
+      case "PRIMARY": {
+        this.pCustomer.job = "Realschulabschluss";
+        break;
+      }
+      case "SECONDARY": {
+        this.pCustomer.job = "Abitur";
+        break;
+      }
+      case "TERTIARY": {
+        this.pCustomer.job = "Universitätsabschluss";
+        break;
+      }
+      case "UNKNOWN": {
+        this.pCustomer.job = "Unbekannt";
+        break;
+      }
+    }
+    this.pCustomer.birthday = this.customer.birthday.substring(8) + "." + this.customer.birthday.substring(5,7) + "." + this.customer.birthday.substring(0,4);
+
+
   }
 }
